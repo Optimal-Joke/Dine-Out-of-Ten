6b3f1e1557e4e6e6a714040f2fcc3afe417f186b
Hunter Holland <hunterholland@tuparev.com>
2020-11-28 22:15:43 -0500

    Initial Commit

---
+++ Dine Out of Ten/Dine_Out_of_TenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dine_Out_of_TenApp.swift
+//  Dine Out of Ten
+//
+//  Created by Hunter Holland on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct Dine_Out_of_TenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
